4e4a1e4411426f6a6d8ff7b63ad3ae1e9cc47d45
Nada Tamer <nadatameer@outlook.com>
2023-10-31 11:40:26 +0200

    fix number of iterations

--- code/Simulated_annealing.py
+++ code/Simulated_annealing.py
@@ -5,26 +5,28 @@ import matplotlib.pyplot as plt
 
 
 def simulated_annealing(iteration_index,current_solution,current_objective,
-                        initial_temperature,curr_temperature,cooling_rate,linear,
+                        initial_temperature,curr_temperature,cooling_rate,linear,num_iterations,
                         min_v_main,max_v_main,
                         weight_func_1, cars_ramp_no,cars_ramp_merged_no,new_solution_dic,distances_to_merge):
-    #fn+1
-    new_objective = objective_func.objective_func(weight_func_1, cars_ramp_no,cars_ramp_merged_no,new_solution_dic,distances_to_merge, min_v_main,max_v_main)
-    #delta f
-    delta_objective = new_objective - current_objective
+    
+    for _ in range(num_iterations):
+        #fn+1
+        new_objective = objective_func.objective_func(weight_func_1, cars_ramp_no,cars_ramp_merged_no,new_solution_dic,distances_to_merge, min_v_main,max_v_main)
+        #delta f
+        delta_objective = new_objective - current_objective
 
-    if delta_objective >= 0: #we are maximizing so if it is positive, it is a good thing
-        current_solution = new_solution_dic
-        current_objective = new_objective
-    else :
-        #p 
-        k = 30 # multiply by delta objective to make probability change greater 
-        acceptance_prob = (math.e)** (((delta_objective)) / curr_temperature )
-        print("probability is ",acceptance_prob)
-        
-        if acceptance_prob > random.randint(0,1):
+        if delta_objective >= 0: #we are maximizing so if it is positive, it is a good thing
             current_solution = new_solution_dic
-            current_objective = new_objective    
+            current_objective = new_objective
+        else :
+            #p 
+            k = 30 # multiply by delta objective to make probability change greater 
+            acceptance_prob = (math.e)** (((delta_objective)) / curr_temperature )
+            print("probability is ",acceptance_prob)
+            
+            if acceptance_prob > random.randint(0,1):
+                current_solution = new_solution_dic
+                current_objective = new_objective    
     
     if linear :          
         curr_temperature = initial_temperature - cooling_rate*iteration_index
